a57602cc301677c548bdd3c54205bff113de9671
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2024-09-08 11:17:10 -0300

    Criação classe usuário

---
+++ src/main/java/com/ricardo/helpdesk/domain/User.java
@@ -0,0 +1,21 @@
+package com.ricardo.helpdesk.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
