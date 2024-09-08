5f566a7eccbfcebb0367bed828b476319dd1fee4
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2024-09-08 11:47:01 -0300

    Ajustando resource usuário findById

--- src/main/java/com/ricardo/helpdesk/resources/UserResource.java
+++ src/main/java/com/ricardo/helpdesk/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.ricardo.helpdesk.resources;
 
 import com.ricardo.helpdesk.domain.User;
+import com.ricardo.helpdesk.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Ricardo", "ricardo@email.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
