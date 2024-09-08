37e8411c8efa80c079baa89f9fe4be73590f1d04
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2024-09-08 14:52:27 -0300

    Adicionando perfil de teste

---
+++ src/main/java/com/ricardo/helpdesk/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.ricardo.helpdesk.config;
+
+import com.ricardo.helpdesk.domain.Usuario;
+import com.ricardo.helpdesk.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    @Autowired
+    private UsuarioRepository repo;
+
+    @Bean
+    public boolean iniciaDB(){
+        Usuario u1 = new Usuario(null,"Ricardo", "ricardo@email.com", "123");
+        Usuario u2 = new Usuario(null,"Maria", "maria@email.com", "123");
+
+        repo.saveAll(List.of(u1, u2));
+
+        return true;
+    }
+}
